380160140bbcf1b6d58600f735a730287d35ab6b
Robert Hristov <hristov.robert@outlook.com>
2022-03-13 14:41:44 +0200

    One more function added which will be used for playing the game. The game logic is now ready. User interface will be added at later stage.

--- script.js
+++ script.js
@@ -9,35 +9,72 @@ function computerPlay(){
 // let result = computerPlay();
 // console.log(result);
 
+// A function that returns the outcome of a played round.
 function playRound(playerSelection, computerSelection){
+    // Make the input case insensitive.
     let p1 = playerSelection.toLowerCase();
     let p2 = computerSelection.toLowerCase();
 
+    // Check if player 1 or player 2 wins and return an informative string.
     if (p1 == p2){
         return `Both players selected ${p1}. It's a tie!`;
     }
     else if (p1 == "rock"){
         if (p2 == "scissors"){
-            return "Rock smashes scissors! You win!";
+            return "You win!";
         }
         else{
-            return "Paper covers rock! You lose.";
+            return "You lose.";
         }
     }
     else if (p1 == "paper"){
         if (p2 == "rock"){
-            return "Paper covers rock! You win!";
+            return "You win!";
         }
         else{
-            return "Scissors cuts paper! You lose.";
+            return "You lose.";
         }
     }
     else if (p1 == "scissors"){
         if (p2 == "paper"){
-            return "Scissors cuts paper! You win!";
+            return "You win!";
         }
         else{
-            return "Rock smashes scissors! You lose.";
+            return "You lose.";
         }
     }
 }
+
+// The main function which plays the game. 
+function game(){
+    // Result counting variables.
+    let playerResult = 0;
+    let computerResult = 0;
+
+    // A loop which will prompt for five games and will display the result after each game in the console. This loop is 
+    // also used for incrementing the counting variables.
+    for (let i = 0; i < 5; i++){
+        let player = window.prompt("Your turn: ");
+        let computer = computerPlay();
+        let round = playRound(player, computer);
+
+        if (round == "You win!"){
+            playerResult++;
+        }
+        else if (round == "You lose."){
+            computerResult++;
+        }
+
+        console.log(round);
+    }
+
+    // Conditional logic for deciding who is the winner of the game.
+    if(playerResult > computerResult){
+        return "You win!";
+    }
+    else{
+        return "Computer wins!";
+    }
+}
+// Start the game itself which will alert the winner at the end.
+window.alert(game());
