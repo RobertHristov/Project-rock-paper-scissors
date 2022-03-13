aa1c674807eaba5b303457a6a4312b8a26257520
Robert Hristov <hristov.robert@outlook.com>
2022-03-13 13:31:54 +0200

    Comments and tests added.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// A function whic returns randomly eather ‘Rock’, ‘Paper’ or ‘Scissors’.
 function computerPlay(){
+    // A list of values, that will be returned.
     let valuesList = ["Rock", "Paper", "Scissors"];
+    // Return randomly value from valuesList.
     return valuesList[Math.floor(Math.random() * 3)];
 }
+// let result = computerPlay();
+// console.log(result);
