b7783560f7d920fbb56b97d1ebca37d2a046a82c
Robert Hristov <hristov.robert@outlook.com>
2022-03-13 13:59:32 +0200

    Function for calculation the result added in script.js

--- script.js
+++ script.js
@@ -5,5 +5,39 @@ function computerPlay(){
     // Return randomly value from valuesList.
     return valuesList[Math.floor(Math.random() * 3)];
 }
+// Tests:
 // let result = computerPlay();
 // console.log(result);
+
+function playRound(playerSelection, computerSelection){
+    let p1 = playerSelection.toLowerCase();
+    let p2 = computerSelection.toLowerCase();
+
+    if (p1 == p2){
+        return `Both players selected ${p1}. It's a tie!`;
+    }
+    else if (p1 == "rock"){
+        if (p2 == "scissors"){
+            return "Rock smashes scissors! You win!";
+        }
+        else{
+            return "Paper covers rock! You lose.";
+        }
+    }
+    else if (p1 == "paper"){
+        if (p2 == "rock"){
+            return "Paper covers rock! You win!";
+        }
+        else{
+            return "Scissors cuts paper! You lose.";
+        }
+    }
+    else if (p1 == "scissors"){
+        if (p2 == "paper"){
+            return "Scissors cuts paper! You win!";
+        }
+        else{
+            return "Rock smashes scissors! You lose.";
+        }
+    }
+}
